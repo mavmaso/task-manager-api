800b4e46b876ce1c9573caebf32f4603eff67af1
Marcos Soares <mavmaso@gmail.com>
2018-08-22 05:42:58 +0000

    [WIP] host issues

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::UsersController < ApplicationController
+  def show; end
+end
---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'User API', type: :request do
+  let!(:user) { create(:user)}
+  let(:user_id) { user.id }
+
+  before { host! 'api.taskmanager.test'}
+
+  describe 'GET /users/:id' do
+    before do
+      headers = { "Accept" => 'application/vnd.taskmanager.v1'}
+      get "/users/#{user_id}", {}, headers
+    end
+
+    context 'when the user exists' do
+      it 'returns the user' do
+        user_response = JSON.parse(response.body)
+        expect(user_response['id']).to eq(user_id)
+      end
+    end
+  end
+end
