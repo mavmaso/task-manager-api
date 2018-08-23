2c745aaaa5462e2c655e8aa1bace0ab688e99ea3
Marcos Soares <mavmaso@gmail.com>
2018-08-23 17:20:11 +0000

    [WIP] rspec and tune fixes

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -31,4 +31,33 @@ RSpec.describe 'User API', type: :request do
       end
     end
   end
+
+  describe 'POST /users' do
+    before do
+      headers = {'Accept' => 'application/vnd.taskmanager.v1' }
+      post '/users', params: {user: user_params }, headers: headers
+    end
+
+    context 'when the request params are valid' do
+      let(:user_params) { attributes_for(:user) }
+
+      it 'returns status code 201'do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'returns json data for the created user'do
+        user_response = JSON.parse(response.body)
+        expect(user_response['email']).to eq(user_params[:email])
+      end
+    end
+
+    context 'when the request params are not valid' do
+      let(:user_params) { attributes_for(:user, email: 'invalid_email@')}
+
+      it 'returns status code 422' do
+
+      end
+    end
+  end
+
 end
