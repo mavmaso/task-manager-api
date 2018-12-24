cc8638ba5f5b967489b31d128a255c170301ca84
Marcos Soares <mavmaso@gmail.com>
2018-12-24 15:25:01 +0000

    Adding association with tasks for user model and tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,8 @@ RSpec.describe User, type: :model do
   #   it { expect(user).not_to be_valid }
   # end
 
+  it { is_expected.to have_many(:tasks).dependent(:destroy)}
+
   it { is_expected.to validate_presence_of(:email)}
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive}
   it { is_expected.to validate_confirmation_of(:password)}
