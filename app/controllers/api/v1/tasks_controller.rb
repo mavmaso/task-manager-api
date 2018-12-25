4dceee0a52cc722f305bfd89e351cc4038d1b03b
Marcos Soares <mavmaso@gmail.com>
2018-12-25 02:33:51 +0000

    Adding create action to the tasks controller with tests

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -10,4 +10,19 @@ before_action :authenticate_with_token!
     task = current_user.tasks.find(params[:id])
     render json: task, status: 200
   end
+
+  def create
+    task = current_user.tasks.build(task_params)
+    if task.save
+      render json: task, status:201
+    else
+      render json: { errors: task.errors }, status:422
+    end
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title, :description, :deadline, :done)
+    end
 end
